33c1510ee1978ba8433be2989997c55954468590
theRoseOfThePhoenix <sandrinebecaert@gmail.com>
2024-04-13 13:49:47 +0200

    router changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable no-unused-vars */
 // @ts-nocheck
+import { HashRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import Router from "./Router.jsx";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/about/About.jsx";
+import Projet from "./pages/projets/Projet.jsx";
 import Footer from "./components/footer/footer.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Router />
+    <HashRouter>
+      <Routes>
+        <Route path="/projets/:projetId" element={<Projet />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/*" element={<Error />} />  */}
+      </Routes>
       <Footer />
-    </div>
+    </HashRouter>
   );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 // @ts-nocheck
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./styles/Main.scss/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
